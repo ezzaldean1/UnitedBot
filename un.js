09c3591301cd0c5a530637d90dbc9f3835996163
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-09-27 20:45:31 +0300

    Update un.js

--- un.js
+++ un.js
@@ -1162,33 +1162,5 @@ client.on('message', message => {
 
 });
 
-const A7MD = new Discord.Client();
-
-console.log("BOT ONLINE");
-
- 
-
-A7MD.on("guildMemberAdd", member => {
-
-  member.createDM().then(function (channel) {
-
-  return channel.send('**
-
-سلام عليكم 
-
-ممكن تشرفنا بالسيرفر ي بعدي
-
-                               [https://discord.gg/QHHa2Qs] 
-
-الدعووة خاصة لك ................... [ ${member}  ]
-
-**') 
-
-}).catch(console.error)
-
-})
-
-A7MD.login(process.env.BOT_TOKEN);
-
 
 client.login(process.env.BOT_TOKEN);
