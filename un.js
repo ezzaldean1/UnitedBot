4157f9a5e0df3b849e19bb71e4c35147fa0a190f
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-09-28 11:58:18 +0300

    Update un.js

--- un.js
+++ un.js
@@ -1162,5 +1162,399 @@ client.on('message', message => {
 
 });
 
+client.on('voice', (old, now) => {
 
+  const channel = client.channels.get('495156705701920789');
+
+  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
+
+  const size = channel.name.match(/\[\s(\d+)\s\]/);
+
+  if (!size) return channel.setName(`Voice: [${currentSize}]`);
+
+  if (currentSize !== size) channel.setName(`Voice: [${currentSize}]`);
+
+});
+
+
+
+client.on('message', msg => {
+
+    if (msg.content == '-play') {
+
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+
+         msg.member.voiceChannel.join();
+
+     }
+
+    }
+
+}
+
+})
+
+//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+client.on('message', async msg => { // eslint-disable-line
+
+	if (msg.author.bot) return undefined;	//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	if (!msg.content.startsWith(prefix)) return undefined;
+
+	const args = msg.content.split(' ');
+
+	const searchString = args.slice(1).join(' ');
+
+	//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
+
+	const serverQueue = queue.get(msg.guild.id);
+
+//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	let command = msg.content.toLowerCase().split(" ")[0];
+
+	command = command.slice(prefix.length)
+
+//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	if (command === `play`) {
+
+		const voiceChannel = msg.member.voiceChannel;
+
+		if (!voiceChannel) return msg.channel.send('يجب توآجد حضرتك بروم صوتي .');
+
+		const permissions = voiceChannel.permissionsFor(msg.client.user);
+
+		if (!permissions.has('CONNECT')) {
+
+			//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+			return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
+
+		}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		if (!permissions.has('SPEAK')) {
+
+			return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
+
+		}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		if (!permissions.has('EMBED_LINKS')) {
+
+			return msg.channel.sendMessage("**يجب توآفر برمشن `EMBED LINKS`لدي **")
+
+		}
+
+		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+
+			const playlist = await youtube.getPlaylist(url);
+
+			const videos = await playlist.getVideos();
+
+			//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+			for (const video of Object.values(videos)) {
+
+				const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
+
+				await handleVideo(video2, msg, voiceChannel, true); // eslint-disable-line no-await-in-loop
+
+			}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+			return msg.channel.send(` **${playlist.title}** تم الإضآفة إلى قأئمة التشغيل`);
+
+		} else {
+
+			try {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+				var video = await youtube.getVideo(url);
+
+			} catch (error) {
+
+				try {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+					var videos = await youtube.searchVideos(searchString, 5);
+
+					let index = 0;
+
+					const embed1 = new Discord.RichEmbed()
+
+			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
+
+${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
+
+//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
+
+					
+
+					// eslint-disable-next-line max-depth
+
+					try {
+
+						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
+
+							maxMatches: 1,
+
+							time: 15000,
+
+							errors: ['time']
+
+						});//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+					} catch (err) {
+
+						console.error(err);
+
+						return msg.channel.send('لم يتم إختيآر مقطع صوتي');
+
+					}
+
+					const videoIndex = parseInt(response.first().content);
+
+					var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+
+				} catch (err) {
+
+					console.error(err);
+
+					return msg.channel.send(':X: لا يتوفر نتآئج بحث ');
+
+				}
+
+			}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+			return handleVideo(video, msg, voiceChannel);
+
+		}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	} else if (command === `skip`) {
+
+		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
+
+		if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لتجآوزه');
+
+		serverQueue.connection.dispatcher.end('تم تجآوز هذآ المقطع');
+
+		return undefined;
+
+	} else if (command === `stop`) {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
+
+		if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لإيقآفه');
+
+		serverQueue.songs = [];
+
+		serverQueue.connection.dispatcher.end('تم إيقآف هذآ المقطع');
+
+		return undefined;
+
+	} else if (command === `vol`) {
+
+		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
+
+		if (!serverQueue) return msg.channel.send('لا يوجد شيء شغآل.');
+
+		if (!args[1]) return msg.channel.send(`:loud_sound: مستوى الصوت **${serverQueue.volume}**`);
+
+		serverQueue.volume = args[1];//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
+
+		return msg.channel.send(`:speaker: تم تغير الصوت الي **${args[1]}**`);
+
+	} else if (command === `np`) {
+
+		if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
+
+		const embedNP = new Discord.RichEmbed()
+
+	.setDescription(`:notes: الان يتم تشغيل : **${serverQueue.songs[0].title}**`)
+
+		return msg.channel.sendEmbed(embedNP);
+
+	} else if (command === `queue`) {
+
+		//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
+
+		let index = 0;
+
+		//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		const embedqu = new Discord.RichEmbed()
+
+//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+.setDescription(`**Songs Queue**
+
+${serverQueue.songs.map(song => `**${++index} -** ${song.title}`).join('\n')}
+
+**الان يتم تشغيل** ${serverQueue.songs[0].title}`)
+
+		return msg.channel.sendEmbed(embedqu);
+
+	} else if (command === `pause`) {
+
+		if (serverQueue && serverQueue.playing) {
+
+			serverQueue.playing = false;
+
+			serverQueue.connection.dispatcher.pause();
+
+			return msg.channel.send('تم إيقاف الموسيقى مؤقتا!');
+
+		}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		return msg.channel.send('لا يوجد شيء حالي ف العمل.');
+
+	} else if (command === "resume") {
+
+		if (serverQueue && !serverQueue.playing) {
+
+			serverQueue.playing = true;
+
+			serverQueue.connection.dispatcher.resume();
+
+			return msg.channel.send('استأنفت الموسيقى بالنسبة لك !');
+
+		}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		return msg.channel.send('لا يوجد شيء حالي في العمل.');
+
+	}
+
+	return undefined;
+
+});
+
+//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+async function handleVideo(video, msg, voiceChannel, playlist = false) {
+
+	const serverQueue = queue.get(msg.guild.id);
+
+	console.log(video);
+
+	//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+//	console.log('yao: ' + Util.escapeMarkdown(video.thumbnailUrl));
+
+	const song = {
+
+		id: video.id,
+
+		title: Util.escapeMarkdown(video.title),
+
+		url: `https://www.youtube.com/watch?v=${video.id}`
+
+	};//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	if (!serverQueue) {
+
+		const queueConstruct = {
+
+			textChannel: msg.channel,
+
+			voiceChannel: voiceChannel,
+
+			connection: null,
+
+			songs: [],
+
+			volume: 5,
+
+			playing: true
+
+		};//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		queue.set(msg.guild.id, queueConstruct);
+
+//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		queueConstruct.songs.push(song);
+
+//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		try {
+
+			var connection = await voiceChannel.join();
+
+			queueConstruct.connection = connection;
+
+			play(msg.guild, queueConstruct.songs[0]);
+
+		} catch (error) {
+
+			console.error(`I could not join the voice channel: ${error}`);
+
+			queue.delete(msg.guild.id);
+
+			return msg.channel.send(`لا أستطيع دخول هذآ الروم ${error}`);
+
+		}
+
+	} else {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		serverQueue.songs.push(song);
+
+		console.log(serverQueue.songs);
+
+		if (playlist) return undefined;
+
+		else return msg.channel.send(` **${song.title}** تم اضافه الاغنية الي القائمة!`);
+
+	}
+
+	return undefined;
+
+}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+function play(guild, song) {
+
+	const serverQueue = queue.get(guild.id);
+
+	if (!song) {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		serverQueue.voiceChannel.leave();
+
+		queue.delete(guild.id);
+
+		return;//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	console.log(serverQueue.songs);
+
+//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+
+		.on('end', reason => {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+
+			else console.log(reason);
+
+			serverQueue.songs.shift();//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+			play(guild, serverQueue.songs[0]);
+
+		})//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+		.on('error', error => console.error(error));//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+
+	serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
+
+}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
 client.login(process.env.BOT_TOKEN);
