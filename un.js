2db7e30e54f2c63d8d8f9629c129a83c58fb17a4
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-09-28 12:02:49 +0300

    Update un.js

--- un.js
+++ un.js
@@ -1,5 +1,17 @@
 const Discord = require('discord.js');
+const ytdl = require("ytdl-core");
 
+const { Client, Util } = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
 const client = new Discord.Client();
 
 client.on('ready', () => {
